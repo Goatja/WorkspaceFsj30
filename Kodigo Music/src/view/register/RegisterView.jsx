7d58deefd373b311589ef258d0cf6fc11bfc85b5
Goatja <jorge.arana528@gmail.com>
2025-08-29 15:53:47 -0600

    fixed some responsive issues

--- Kodigo Music/src/view/register/RegisterView.jsx
+++ Kodigo Music/src/view/register/RegisterView.jsx
@@ -19,34 +19,27 @@ export const RegisterView = () => {
   });
 
   const onSubmit = (data) => {
-    console.log(data);
+    console.log(data.user);
   };
   return (
     <>
       <div className="wrap-form-container row">
         <section className="col-lg-8 col-md-8 col-sm-12 text-light m-auto">
           <h1 className="text-center">Kody Music</h1>
-          <p className="text-center fs-4 lh-3 shadow">
+          <p className="text-center fs-4 lh-3 shadow movil">
             Sumérgete en el ritmo que mueve al mundo. En Kodigo Music, la música
-            no solo se escucha—se vive. Descubre los éxitos que están marcando
-            tendencia, desde los beats electrizantes de Dua Lipa hasta las
-            melodías inolvidables de Ed Sheeran y The Weeknd. Explora playlists
-            curadas, conoce nuevos artistas, y deja que cada canción te acompañe
-            en tu día. Ya sea que estés buscando inspiración, energía o
-            simplemente una buena vibra, aquí encontrarás el sonido perfecto
-            para cada momento. Regístrate hoy y forma parte de una comunidad que
-            vibra al compás de la música. Porque en Kodigo Music, cada nota
+            no solo se escucha—se vive.
             cuenta.
           </p>
         </section>
         <section className="col-lg-4 col-md-4 col-sm-12">
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="form p-3 color-fondo-secundario rounded text-light"
+            className="formulario p-3 color-fondo-secundario rounded text-light"
           >
             <h1 className="text-center fw-bolder fs-1 text-light">Register</h1>
             <label htmlFor="" className="form-label">
-              Usuario
+              User
             </label>
             <input className="form-control" {...register("usuario")} />
             <label htmlFor="" className="form-label">
