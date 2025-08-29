48db90e5e8d17c6f5e3e4a52695ec4e17a311108
Goatja <jorge.arana528@gmail.com>
2025-08-28 23:21:18 -0600

    styles

--- Kodigo Music/src/view/homemusic/components/Cards.jsx
+++ Kodigo Music/src/view/homemusic/components/Cards.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import { useState } from "react";
 
-export const Cards = ({artist, title, image}) => {
+
+export const Cards = ({ artist, title, image }) => { 
+
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
   return (
     <>
-        <div
-            className="card text-white bg-dark col-md-3 col-lg-3 col-sm-12 m-1"
-        >
-            <img className="card-img-top h-50" src={image} alt={artist} />
-            <div className="card-body">
-                <h4 className="card-title">{title}</h4>
-                <p className="card-text">{artist}</p>
-            </div>
-            <span><i className="bi bi-play-circle-fill text-info rounded fs-1"></i></span>
+      <div onMouseEnter={() => setIsMouseOver(true)} 
+           onMouseLeave={() => setIsMouseOver(false)}
+      className="card text-white bg-dark col-md-3 col-lg-3 col-sm-12 m-1">
+        <img className="card-img-top h-50" src={image} alt={artist} />
+        <div className="card-body">
+          <h4 className="card-title">{title}</h4>
+          <p className="card-text">{artist}</p>
         </div>
+  
+          {isMouseOver && <i className="bi bi-play-circle-fill text-info rounded fs-2"></i>  }
         
+      </div>
     </>
-  )
-}
+  );
+};
