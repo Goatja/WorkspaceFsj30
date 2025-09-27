c8f0ca0bb0cb5d81f9eb4fb236ce352bcb93e5f9
Goatja <jorge.arana528@gmail.com>
2025-09-27 16:40:14 -0600

    comments

--- phpPract/pract/replacestr.php
+++ phpPract/pract/replacestr.php
@@ -1,19 +1,30 @@
 <?php 
 
 //Reemplezar letras en una cadena de texto.
+
 $txtToReplace = "examen extraordinario de expresividad";
 
 function replaceStr($str): string{
+    
    for($i = 0; $i < strlen($str); $i++){
         if($str[$i] === "x"){
-            $str[$i] = "s";
+            $str[$i] = "s"; /*Esto no esta bien ya que las cadenas en php son inmutables 
+                                aunque en este caso es permitido pero puede llagar a causar algunos
+                                problemas.
+                                Por que me permite hacer un aparente cambio entonces?
+                                Tecnicamente porque no se esta cambiando directamente la referencia del str sino una copia 
+                                temporal mienstras existe la funcion.
+                            */
         }
     }
     return $str;
 }
 
-$result = replaceStr($txtToReplace);
+$result = replaceStr($txtToReplace);//Puede resultar en algunos problemas
 
 echo $result;
-echo str_replace("e", "a", $txtToReplace);
+echo "\n";
+echo str_replace("e", "a", $txtToReplace);//Esta seria una solucion bastante adecuada.
+
+
 
